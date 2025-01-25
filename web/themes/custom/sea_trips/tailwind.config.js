b127a08f36e9f8d3d76977a1f3f056739b7552e4
Piotr Glaza <daftu44@gmail.com>
2025-01-25 15:38:03 +0100

    Working slider proto

--- web/themes/custom/sea_trips/tailwind.config.js
+++ web/themes/custom/sea_trips/tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ['./templates/**/*.twig', './../../../modules/custom/custom_extras/src/Plugin/Block/*.php',
-    './../../../modules/custom/three_footer/templates/*.twig'],
+    './../../../modules/custom/three_footer/templates/*.twig',
+  './templates/custom/sea_trips/sea_trips.theme'],
     theme: {
       extend: {
+        backgroundImage: {
+          'footer-bg': "url('../images/footer_background.jpeg')",
+        },
+        borderRadius: {
+          '4xl': '40px'
+        },
         colors: {
           'accent-dark-color': '#275CA8',
           'accent-light-color': "#C1D4EF",
@@ -13,7 +20,14 @@ module.exports = {
           'secondary-btn-color': '#EBE1AA',
           'secondary-btn-hov-color': '#FBF1BA',
           'infoicon': '#BCE3FF',
-          'infophoto-link': '#568AC6'
+          'infophoto-link': '#568AC6',
+          'pricing-bg-1': '#F6F5EF',
+          'pricing-btn-1': '#3E2F21',
+          'pricing-bg-2': '#EFF4F6',
+          'pricing-btn-2': '#385364',
+          'pricing-bg-3': '#F6EFF2',
+          'pricing-btn-3': '#643838'
+
   
         },
         fontFamily: {
@@ -22,5 +36,13 @@ module.exports = {
         },
       },
     },
+    safelist: [
+      'bg-pricing-bg-1',
+      'bg-pricing-bg-2',
+      'bg-pricing-bg-3',
+      'bg-pricing-btn-1',
+      'bg-pricing-btn-2',
+      'bg-pricing-btn-3',
+    ],
     plugins: [],
   }
