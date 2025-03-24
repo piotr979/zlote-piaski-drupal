2a94bf7c1c3673d0fdbf106817170fb6e2f14415
Piotr Glaza <daftu44@gmail.com>
2025-03-24 19:10:40 +0100

    Gitignore update

--- web/modules/contrib/imageapi_optimize_binaries/src/ImageAPIOptimizeShellOperationsInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Drupal\imageapi_optimize_binaries;
-
-interface ImageAPIOptimizeShellOperationsInterface {
-  public function findExecutablePath($executable = NULL);
-  public function execShellCommand($command, $options, $arguments);
-  public function saveCommandStdoutToFile($cmd, $dst);
-}
